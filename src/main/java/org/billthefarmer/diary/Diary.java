93855f59077ea58e260bcef1ca26009e69c91983
Bill Farmer <williamjfarmer@yahoo.co.uk>
2017-06-07 12:00:43 +0100

    Add MarkdownView library

--- src/main/java/org/billthefarmer/diary/Diary.java
+++ src/main/java/org/billthefarmer/diary/Diary.java
@@ -64,6 +64,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
+import org.billthefarmer.markdown.MarkdownView;
+
 // Diary
 public class Diary extends Activity
     implements DatePickerDialog.OnDateSetListener
